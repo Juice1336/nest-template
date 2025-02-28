3f5fb0ac9c29b555c2b52bda23214050017a7e8a
Night King <rxarinze@live.com>
2025-02-28 17:33:58 +0100

    clean-up

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
 import { UsersService } from './users/users.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from 'config/configuration';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      entities: [User],
-      synchronize: true,
-      ssl: false,
-      url: '',
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      name: 'default',
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DB_URL'),
+        entities: [User],
+        synchronize: true,
+        ssl: false,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { rateLimiter } from 'middleware/ratelimit.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  app.use(helmet());
+  app.use(rateLimiter);
   await app.listen(3000);
 }
 bootstrap();
